10c09bd2119c8019a2a88b2c2ea65c3520196315
Nasruddin Shaik <sknasruu@gmail.com>
2022-06-07 09:59:58 +0530

    Created first test

---
+++ src/test/java/com/learn/tests/LoginTest.java
@@ -0,0 +1,11 @@
+package com.learn.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void displayMessageToConsole() {
+		System.out.println("Hello World!");
+	}
+}
