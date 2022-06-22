4d1abbedba2449c7b1a433fb655aa1974b704ef5
shaik_n <shaik_n@HYHC-71888-WL.India.XoriantCorp.com>
2022-06-21 23:31:37 -0700

    added sum method.

--- src/test/java/com/learn/tests/LoginTest.java
+++ src/test/java/com/learn/tests/LoginTest.java
@@ -3,9 +3,18 @@ package com.learn.tests;
 import org.testng.annotations.Test;
 
 public class LoginTest {
-	
+
 	@Test
 	public void displayMessageToConsole() {
 		System.out.println("Hello World!");
+	}
+
+	@Test
+	public void sum() {
+		int a, b;
+		a = 100;
+		b = 200;
+		System.out.println(a + b);
+
 	}
 }
