915a4e70384a65e10a9eed0781e1d6177c8fd872
shaik_n <shaik_n@HYHC-71888-WL.India.XoriantCorp.com>
2022-06-22 06:56:35 -0700

    Revert "new commit message"
    This reverts commit c0c4994bd3b14223509d6680af9bf5c1d17fe735.

--- src/test/java/com/learn/tests/FileExplorer.java
+++ src/test/java/com/learn/tests/FileExplorer.java
@@ -3,7 +3,8 @@ package com.learn.tests;
 public class FileExplorer {
 
 	public static void main(String[] args) {
-		System.out.println("Alisha, what are you doing?");
+		// TODO Auto-generated method stub
+
 	}
 
 }
