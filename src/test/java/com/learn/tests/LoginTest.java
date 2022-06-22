c915918019427973f6e1bf467d8c2b017f62f3cd
shaik_n <shaik_n@HYHC-71888-WL.India.XoriantCorp.com>
2022-06-22 00:22:52 -0700

    Created test case from main branch

--- src/test/java/com/learn/tests/LoginTest.java
+++ src/test/java/com/learn/tests/LoginTest.java
@@ -3,9 +3,14 @@ package com.learn.tests;
 import org.testng.annotations.Test;
 
 public class LoginTest {
-	
+
 	@Test
 	public void displayMessageToConsole() {
 		System.out.println("Hello World!");
+	}
+
+	@Test
+	public void testGit() {
+		System.out.println("This is created from main branch");
 	}
 }
