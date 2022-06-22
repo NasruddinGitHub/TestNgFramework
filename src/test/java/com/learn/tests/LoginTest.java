af546741c8a76d638f18abf059617a5bebb79ec8
shaik_n <shaik_n@HYHC-71888-WL.India.XoriantCorp.com>
2022-06-22 07:14:42 -0700

    Remote tracking branch

--- src/test/java/com/learn/tests/LoginTest.java
+++ src/test/java/com/learn/tests/LoginTest.java
@@ -3,9 +3,10 @@ package com.learn.tests;
 import org.testng.annotations.Test;
 
 public class LoginTest {
-	
+
 	@Test
 	public void displayMessageToConsole() {
 		System.out.println("Hello World!");
+		System.out.println("Upstream");
 	}
 }
