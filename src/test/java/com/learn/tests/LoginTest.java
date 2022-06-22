cff367c49f911b5ecd38708a45a97c853452010e
shaik_n <shaik_n@HYHC-71888-WL.India.XoriantCorp.com>
2022-06-22 01:08:31 -0700

    Added new test

--- src/test/java/com/learn/tests/LoginTest.java
+++ src/test/java/com/learn/tests/LoginTest.java
@@ -11,6 +11,12 @@ public class LoginTest {
 	}
 
 	@Test
+	public void displayDuplicateMessageToConsole() {
+		System.out.println("Hello World!");
+		System.out.println("Created from egit");
+	}
+
+	@Test
 	public void testGit() {
 		System.out.println("This is created from main branch");
 		System.out.println("Added this line from github");
