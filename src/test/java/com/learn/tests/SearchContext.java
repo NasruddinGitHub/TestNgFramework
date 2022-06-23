c6ad7149288dbc541a7b2df11c6253974d8caf2d
shaik_n <shaik_n@HYHC-71888-WL.India.XoriantCorp.com>
2022-06-22 22:59:52 -0700

    Without author name

--- src/test/java/com/learn/tests/SearchContext.java
+++ src/test/java/com/learn/tests/SearchContext.java
@@ -3,6 +3,7 @@ package com.learn.tests;
 public class SearchContext {
 
 	public static void main(String[] args) {
+		
 		System.out.println("RemoteWebDriver is a class that implements WebDriver");
 		System.out.println("Search Context");
 		System.out.println("SearchContext is the super interface in Selenium Webdriver");
