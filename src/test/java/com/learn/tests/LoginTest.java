ecf4c7e8947294d11c35600aac4a59821fbbd10a
shaik_n <shaik_n@HYHC-71888-WL.India.XoriantCorp.com>
2022-06-23 00:34:18 -0700

    Push via remote

--- src/test/java/com/learn/tests/LoginTest.java
+++ src/test/java/com/learn/tests/LoginTest.java
@@ -6,21 +6,6 @@ public class LoginTest {
 
 	@Test
 	public void displayMessageToConsole() {
-		System.out.println("Checking git pull.. one more time in egit");
-		System.out.println("Checking git pull.. in egit");
-		System.out.println("Checking git pull after git fetch");
-		System.out.println("Testing git fetch and merge once from the eclipse");
-		System.out.println("Testing git pull one more time from the eclipse");
-		System.out.println("Testing git pull from the eclipse");
-		System.out.println("Hello World!");
-		System.out.println("Upstream");
-		System.out.println("Local");
-		System.out.println("Test");
-		System.out.println("Pushing from history");
-		System.out.println("Push to upstream");
-		System.out.println("Push to remote tracking");
-		System.out.println("Push branch");
-		System.out.println("Changes");
-		System.out.println("Testable");
+		System.out.println("Testing push via remote option");
 	}
 }
